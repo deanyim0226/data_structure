bc725bc011e5aac19d2a178294b946b07063c327
ChaesunYim <97508288+ChaesunYim@users.noreply.github.com>
2023-07-04 19:26:46 -0700

    failed..

--- linkedlist.py
+++ linkedlist.py
@@ -29,31 +29,102 @@ class Linkedlist():
     append data into linked list 
     """
     def append(self,data):
-        return 0
-    
+
+        if self.size == 0:
+             head = Node(data)
+             node = head.next
+             self.size += 1
+        
+        else:
+             
+             head = self.head
+
+             while head.next != None:
+                  
+                  head = head.next
+            
+        node = Node(data)
+        head.next = node
+        self.size += 1
+
+
+
+
+
+
     """
     remove data from linked list if the data is not present, throw an error message
     """
+       
 
     def remove(self,data):
-        return 0
+        
+        removeNode = None
+        nextNode = None
+        head = self.head
+
+        while head.data != None:
+                
+            if head.data == data:      
+                removeNode = head
+                head = head.next
+                removeNode = None
+
+        while head.next != None:
+             nextNode = head
+        
+        
+
+        
+
+
+
+                  
     
     """
     get data or element associated with a given index from the linked list 
     """
     def getDatabyIndex(self,index):
-        return 0
+
+        ptr=0
+        ptr = self.head
+        head = self.head.next
+        value = self.head.data
+
+        
+        while head != None:    
+             
+             if ptr == index:
+                  return value
+             
+        
+
+             
+             
     
     """
     return the size of linked list
     """
 
     def size(self):
-        return 0
+
+        sizeOflist = 0
+        self.sizeOflist = self.size
+        return sizeOflist
     
     """
     display all the elements in linked list 
     """
     def display(self):    
-        return 0
+        
+        display = self.head
+
+        while display != None:
+             print(display.data) 
+
+             display = self.head.next
+
+
+
+
 
