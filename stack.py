ae8728524dd6cfad218fd8b3d2123dd34a234819
Dean Yim <donghyeon0226@gmail.com>
2023-07-06 01:32:21 -0700

    add stack ds

---
+++ stack.py
@@ -0,0 +1,35 @@
+class Stack:
+        # your job is to implement stack data structure by using linkedlist that you
+        # previously implemented. Keep in mind that you can always modify and change your linkedlist
+        # as needed 
+
+    def __init__(self) -> None:
+        # add your instance variables
+    
+        pass
+
+        # add an element to the top of a stack 
+
+    def push(element):
+
+        return 0
+
+        # remove an element from the top of a stack
+        # when you are trying to pop more than the size of stack
+        # then you should throw an error 
+        #  
+    def pop():
+
+        return 0
+
+        # check whether the stack is empty or not
+    def isEmpty():
+
+        return False
+
+        # get the value of the top element without removing it
+
+    def peek():
+
+        return 0
+    
