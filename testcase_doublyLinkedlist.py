f3800bc0aef8887ae716c26e9aec1c6514e01c53
Dean Yim <donghyeon0226@gmail.com>
2023-07-09 00:55:18 -0700

    add doublyLinkedlist solution

--- testcase_doublyLinkedlist.py
+++ testcase_doublyLinkedlist.py
@@ -1,11 +1,11 @@
 import unittest
-import doublyLinkedlist
+import solution_doublyLinkedlist
 
 class Test(unittest.TestCase):
 
     def setUp(self):
         self.ex = [None,1,4,5,7,8]
-        self.linkedList = doublyLinkedlist.Linkedlist()
+        self.linkedList = solution_doublyLinkedlist.Linkedlist()
         pass
 
     def tearDown(self):
@@ -14,15 +14,18 @@ class Test(unittest.TestCase):
 
     def testAppendToEnd(self):
 
-
         self.linkedList.appendToEnd(1)
         self.linkedList.appendToEnd(4)
         self.linkedList.appendToEnd(5)
         self.linkedList.appendToEnd(7)
         self.linkedList.appendToEnd(8)
+        self.linkedList.appendToFirst(10)
 
+        self.linkedList.remove(1)
 
         self.linkedList.displayFromFirstElement()
+        print("-----------------------------------")
+        self.linkedList.displayFromLastElement()
     
 if __name__ == "__main__":
     unittest.main()
