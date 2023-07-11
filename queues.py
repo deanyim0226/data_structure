33ddeddcd14bf4b2df3ee56b3b2bf3db3073d237
ChaesunYim <97508288+ChaesunYim@users.noreply.github.com>
2023-07-11 14:59:34 -0700

    queues

--- queues.py
+++ queues.py
@@ -27,93 +27,85 @@ class Queue:
         self.size = 0
 
         pass
-
-    # return the first element in queue without removing
-
-    def front(self,data):
-
-        firstNode = self.head.next
-
-        if firstNode.data is data:
-            return firstNode.data
-
         
+  # return the first element in queue without removing
 
+    def peak(self):
 
-    def peek(self):
+        firstNode = self.head
 
-        return 0
+        if self.size > 0:
+            return firstNode.data
+        
+        else:
+            -1
+    
     # enqueue element meaning append element to the end of queue
 
     def enqueue(self,data):
-        size = self.size
+      
 
-        if size == 0:
+        if self.size == 0:
             newNode = Node(data)
-            self.head.next = newNode
-            self.tail.prev = newNode
-            self.tail.next = self.head
+            self.head = newNode
+            self.tail = newNode
 
-            size += 1
+            self.size += 1
+            
         
         else:
-
-            currNode = self.head.next
             newNode = Node(data)
+            tailNode = self.tail
+            tailNode.next = newNode
+            self.tail = newNode
+                
+            self.size += 1
+                
 
-            if newNode.data == data:
-                self.head.next = newNode
-                newNode.next = currNode
-                self.tail.prev = currNode
-                self.tail.next = newNode
-            
-
-        return 0
     
     # dequeue element meaning remove the first element of queue
     def dequeue(self):
-        size = self.size
-        currNode = self.head.next
-        
-        if size == 0:
-            currNode = None
+          
+        if self.size > 0:
+
+            if self.size == 1:
+
+                firstNode = self.head   
+                self.head = None
+                self.tail = None
+                self.size -= 1
+
+                return firstNode.data
+                
+      
+
+            else:
+                    
+                firstNode = self.head
+                self.head = firstNode.next
+                self.size -= 1
+
+                return firstNode.data
         
         else:
-            while currNode:
-                if self.tail.prev:
-                    lastNode = self.tail.prev
-                    temp = lastNode.prev
-                    lastNode = None
-                    size -= 1
-                    self.tail.prev = temp
-                    self.tail.next = self.head
-                    
+            return -1
 
-        return 0
+            
+
+                    
 
     #display elements in queue 
     def display(self):
-        size = self.size
-        currNode = self.head
+        
+        node = self.head
 
-        if size == 0:
-            return None
-        else:
-            while currNode.next is not None:
-                self.tail.prev = currNode
-                currNode = currNode.next
-                self.tail.next = self.head
-                print(currNode)
-                currNode = self.head
-            
-        return 0
+        while node:
+            print(node.data)
+
+        node = node.next
+
+    
     # return the size of queue
     def length(self):
-        size = self.size
-        currNode = self.head
 
-        while currNode.next is not None:
-            currNode = currNode.next
-            size += 1
-
-        return size    
+        return self.size
